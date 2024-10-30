3fcbfa39a53559d4f24c7f233b29416c8587243a
KaushalBdrKunwar <kaushalbahadurkunwar@gmail.com>
2024-10-30 14:15:56 +0545

    conditionals in Python

---
+++ Chapter6/02_if_elif_else_ladder.py
@@ -0,0 +1,16 @@
+a = int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are above the age of consent")
+    print("good for you!")
+
+elif(a<0):
+    print("You are entering an invalid age")
+
+elif(a==0):
+    print("You are entering 0")
+    
+else:
+    print("You are below the age of consent")
+
+print("End of Program")
