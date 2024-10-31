8e208747445a3d19f35e081c433117f65a731a59
KaushalBdrKunwar <kaushalbahadurkunwar@gmail.com>
2024-10-31 10:29:49 +0545

    Loops in Python

---
+++ Chapter7/05_for_loop_iterate.py
@@ -0,0 +1,17 @@
+
+##For Loop with Lists
+l = [1,45,23,5,6,6]
+
+for i in l:
+    print(i)
+
+##For Loop with Tuples
+t = (6,231,75,122)
+for i in t:
+    print(i)
+
+##For Loop with Strings
+s = "Harry"
+
+for i in s:
+    print(i)
---
+++ Chapter7/06_for_with_else.py
@@ -0,0 +1,8 @@
+l = [1,7,8]
+
+for item in l:
+    print(item)
+
+else:
+    print("done") #this occurs when for loop is exhausted
+                  #Isnt used often in real world.
