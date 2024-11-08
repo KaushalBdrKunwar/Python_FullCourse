5070f0a3e07d7193b7982c1911a9080ac61d19c6
KaushalBdrKunwar <kaushalbahadurkunwar@gmail.com>
2024-11-08 14:34:47 +0545

    Super Key in Inheritance

--- Chapter11/04_super.py
+++ Chapter11/04_super.py
@@ -1,4 +1,6 @@
 class Employee:
+    def __init__(self):
+        print("Constructor of Employee")
 
     company = "Infosis"
 
@@ -7,12 +9,20 @@ class Employee:
 
 
 class Programmer(Employee):
+    def __init__(self):
+        super().__init__()
+        print("Constructor of Programmer")
+
     language = "Python"
 
     def getLanguage(self):
-        print(f"The employee know {self.language}")
+        print(f"The employees know {self.language}")
 
 class Manager(Programmer):
+    def __init__(self):
+        super().__init__()
+        print("Constructor of Manager")
+
     print("I know everything.")
 
 m = Manager()
